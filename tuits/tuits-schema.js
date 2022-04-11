6271ec6776664a568d7e1482d15247a2e1076e3e
rahulbaddam13 <rahulrdd007@gmail.com>
2022-04-11 17:47:42 -0400

    a9 js sp22

---
+++ tuits/tuits-schema.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema({
+  tuit: String,
+  likes: Number,
+  topic :String,
+  handle:String,
+  time:String,
+  postedBy: {
+    username: String
+  }
+
+
+}, {collection: 'tuits'});
+export default schema;
