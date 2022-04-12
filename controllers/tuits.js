69cc28b0bfeddaa61bbf82b973c17c201a56be96
rahulbaddam13 <rahulrdd007@gmail.com>
2022-04-11 20:53:40 -0400

    a9 js sp22 seven

--- controllers/tuits.js
+++ controllers/tuits.js
@@ -11,7 +11,7 @@ export default[
     "time": "2h",
     "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
     "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "attachments": {
+    "attachments":  {
       "video": "unKvMC3Y1kI"
     },
     "logo-image": "https://www.rapiddg.com/sites/default/files/imce-files/react.png",
